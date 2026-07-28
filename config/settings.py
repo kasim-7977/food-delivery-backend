758c8f512306f9a4ad410f409972e81a60aea5d0
kasim-7977 <mohammedkasim7977@gmail.com>
2026-07-28 12:45:45 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-orst#hp6_w!w4chm5nien&a@l@$5dw9w4s6@)6mv)tp&#a*qeh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
-    "https://food-delivery-api-cyuc.onrender.com",
+    "food-delivery-api-cyuc.onrender.com",
     "localhost",
     "127.0.0.1",
 ]
