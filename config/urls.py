f20b7b6a68c23fad44d1f1fe8db8fc2b5764dcce
kasim-7977 <mohammedkasim7977@gmail.com>
2026-07-31 11:25:19 +0530

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -24,7 +24,8 @@ urlpatterns = [
     path('api/', include('users.urls')),
     path('api/', include('food.urls')),
 ]
-urlpatterns += static
-(settings.MEDIA_URL,
- document_root=settings.MEDIA_ROOT
+
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
 )
