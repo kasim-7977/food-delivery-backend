627495f947d8834a596468dde1a469d9a2a49d3f
kasim-7977 <mohammedkasim7977@gmail.com>
2026-07-31 12:29:53 +0530

    Update models.py

--- food/models.py
+++ food/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
@@ -20,7 +19,7 @@ class Food(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=8, decimal_places=2)
-    image = CloudinaryField("image")
+    image = models.ImageField(upload_to="foods/")
     available = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     
